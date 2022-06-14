32db464932318950f5398a955055ca2e22371822
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-14 18:38:12 +0530

    Added :Welcome message

---
+++ src/com/bridgelabz/oops/EmployeeWage.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.oops;
+
+public class EmployeeWage {
+    static void welcomeMessage() {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+
+    public static void main(String[] args) {
+        welcomeMessage();
+    }
+
+}
