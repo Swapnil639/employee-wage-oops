9eaadbfc3be6f830f05c086d8ef5e7f8fd8f7f25
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-14 19:08:15 +0530

    Added :Calculate the monthly wages

--- src/com/bridgelabz/oops/EmployeeWage.java
+++ src/com/bridgelabz/oops/EmployeeWage.java
@@ -3,45 +3,37 @@ package com.bridgelabz.oops;
 public class EmployeeWage {
     static final int IS_FULL_TIME = 1;
     static final int Is_PART_TIME = 2;
-    static final int EMP_WAGE_PER_HOUR = 20;
+    static final int EMP_RATE_PER_HOUR = 20;
+    static final int NUM_0F_WORKING_DAYS = 20;
 
-    static void switchCaseStatement() {
+    static void monthlyEmployeeWage() {
         int empHrs = 0;
         int empWage = 0;
-        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+        int totalEmpWage = 0;
 
-        if (empCheck == 1)
-            System.out.println("Full Time");
-        else if (empCheck == 2)
-            System.out.println("Part Time");
-        else
-            System.out.println("Employee Absent");
+        for (int i = 1; i <= NUM_0F_WORKING_DAYS; i++) {
+            int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 
-        switch (empCheck)
-        {
-            case IS_FULL_TIME :
-                empHrs=8;
-                break;
-            case Is_PART_TIME :
-                empHrs=4;
-                break;
-            default:
-                empHrs=0;
+            if (empCheck == IS_FULL_TIME)
+                empHrs = 8;
+            else if (empCheck == Is_PART_TIME)
+                empHrs = 4;
+            else
+                empHrs = 0;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            System.out.println("Day " + i + " :" + empWage);
         }
-
-        empWage = empHrs * EMP_WAGE_PER_HOUR;
-        System.out.println("Employee Hours :" + empHrs);
-        System.out.println("Employee Wage :" + empWage);
+        System.out.println("Total Employee Wage :" + totalEmpWage);
 
     }
 
-    public static void main(String[] args)
-    {
-        switchCaseStatement();
+
+    public static void main(String[] args) {
+        monthlyEmployeeWage();
     }
 
 }
-
 
 
 
